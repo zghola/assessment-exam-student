1b25abc54b27b58a68c1a5cedf1c8a6573a9c1bb
Khurram Virani <kvirani@gmail.com>
2019-06-27 16:25:18 -0700

    12w related fixes (new lint file, etc)
    - New lint rules (based on new style guide and eslint file). See https://web.compass.lighthouselabs.ca/1d7a7164-19ef-4e27-92bb-3d5bb7a2b047
    - Updated eslint package to latest (6.0.1)
    - Fixed various spacing issues (though we now have other lint errors in our test running code, to be fixed as part of a later PR)

--- test-runner.js
+++ test-runner.js
@@ -1,6 +1,7 @@
-"use strict"
+"use strict";
 const crypto  = require("crypto");
 const eslint  = require("eslint");
+console.log('version: ', eslint.version);
 const fs      = require("fs");
 const Mocha   = require("mocha");
 
@@ -15,40 +16,40 @@ class TestRunner {
     const codeFile = this.getCodeFile(questionNumber);
     const code = fs.readFileSync(codeFile, "utf8");
     const examData = JSON.parse(fs.readFileSync(".exam-data", "utf8"));
-    
+
     const results = {
       examId:         examData.examId,
-      questionNumber: parseInt(questionNumber), 
+      questionNumber: parseInt(questionNumber),
       lintResults:    this.runLint(code),
       testResults:    null, // Mocha, next step
       testFileHash:   null,
       studentCode:    code,
       errors:         []
     };
-    
+
     try {
       const testFile = this.getTestFile(questionNumber);
       results.testFileHash = this.getFileHash(testFile);
-      
+
       this.runMocha(questionNumber)
         .then((mochaResults) => {
-            results.testResults = mochaResults.stats;
-            cb(null, results);
-          },
-          (err) => {
-            results.errors.push(err);
-            console.error(err.stack);
-            cb(err, results);
-          });
+          results.testResults = mochaResults.stats;
+          cb(null, results);
+        },
+        (err) => {
+          results.errors.push(err);
+          console.error(err.stack);
+          cb(err, results);
+        });
     } catch (e) {
       cb(e, results);
     }
   }
 
   runLint(code) {
-    const lintOptions = { 
+    const lintOptions = {
       rules: LINT_RULES,
-      globals: { module: {} },
+      globals: { module: 'writable' },
       env: {
         "es6": true,
         "node": true
@@ -56,7 +57,7 @@ class TestRunner {
     };
     return eslint.linter.verify(code, lintOptions);
   }
-  
+
   runMocha(questionNumber, cb) {
     const testFile = this.getTestFile(questionNumber);
     return new Promise((resolve, reject) => {
@@ -68,25 +69,25 @@ class TestRunner {
         });
     });
   }
-  
+
   getCodeFile(questionNumber) {
     const paddedNumber = this.padNumber(questionNumber, 2);
     return `./answers/${paddedNumber}.js`;
   }
-  
+
   getTestFile(questionNumber) {
     const paddedNumber = this.padNumber(questionNumber, 2);
     return `./tests/test_${paddedNumber}.js`;
   }
-  
+
   padNumber(num, length) {
-    let s = num+"";
+    let s = num + "";
     while (s.length < length) {
       s = "0" + s;
     }
     return s;
   }
-  
+
   getFileHash(filePath) {
     const fileContents = fs.readFileSync(filePath, "utf8");
     return crypto.createHash("md5").update(fileContents).digest("hex");
