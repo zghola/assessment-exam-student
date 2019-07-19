bd7cc3543ba28ed541f4894bbefd649cce95b984
David Mills <dave@1morepx.com>
2019-07-19 14:03:45 -0400

    Don't print linting results when linting is disabled
    Also some style fixes

--- index.js
+++ index.js
@@ -17,6 +17,53 @@ if (!questionNumber) {
   process.exit(1);
 }
 
+const printTestStart = function(questionNumber) {
+  console.log(`Running Tests for Question ${questionNumber}`);
+  console.log("------------");
+};
+
+const printLintResults = function(lintResults) {
+  // Skip this if linting is disabled, i.e. lintResults is null
+  if (lintResults === null) {
+    return;
+  }
+
+  console.log("Lint Results");
+  console.log("------------");
+  if (lintResults.length === 0) {
+    console.log("(Everything appears to OK)");
+    console.log("\n");
+    return;
+  }
+  for (let i = 0; i < lintResults.length; i++) {
+    let result = lintResults[i];
+    console.log(`${i + 1}) ${result.message} (Line ${result.line}, Column ${result.column})`);
+  }
+  console.log("\n");
+};
+
+const printSubmissionResponse = function(responseBody) {
+  console.log("Overall Score");
+  console.log("------------");
+  for (let i = 0; i < responseBody.scores.length; i++) {
+    let question = responseBody.scores[i];
+    console.log(`Q${question.questionNumber}. ${question.score}/${question.maxScore}`);
+  }
+  if (responseBody.remainingTime > 0) {
+    const hours = Math.floor(responseBody.remainingTime / 60);
+    const minutes = Math.floor(responseBody.remainingTime % 60);
+    console.log(`\nTime Remaining: ${hours}h${minutes}m\n`);
+  } else {
+    console.log("\nTime Remaining: None (Submission still accepted)\n");
+  }
+};
+
+const printSubmissionError = function(err) {
+  console.error(`ERROR: (${err.code})\n`);
+  console.info(err);
+  console.warn("\nYour test results have NOT been submitted!\n");
+};
+
 const testRunner = new TestRunner();
 const apiClient = new APIClient();
 
@@ -33,44 +80,4 @@ testRunner.run(questionNumber, (err, results) => {
   });
 });
 
-function printTestStart(questionNumber) {
-  console.log(`Running Tests for Question ${questionNumber}`);
-  console.log("------------");
-}
 
-function printLintResults(lintResults) {
-  console.log("Lint Results");
-  console.log("------------");
-  if (lintResults.length == 0) {
-    console.log("(Everything appears to OK)")
-    console.log("\n");
-    return;
-  }
-  for (let i=0; i<lintResults.length; i++) {
-    let result = lintResults[i];
-    console.log(`${i+1}) ${result.message} (Line ${result.line}, Column ${result.column})`);
-  }
-  console.log("\n");
-}
-
-function printSubmissionResponse(responseBody) {
-  console.log("Overall Score");
-  console.log("------------");
-  for (let i=0; i<responseBody.scores.length; i++) {
-    let question = responseBody.scores[i];
-    console.log(`Q${question.questionNumber}. ${question.score}/${question.maxScore}`);
-  }
-  if (responseBody.remainingTime > 0) {
-    const hours = Math.floor(responseBody.remainingTime/60);
-    const minutes = Math.floor(responseBody.remainingTime%60);
-    console.log(`\nTime Remaining: ${hours}h${minutes}m\n`);
-  } else {
-    console.log("\nTime Remaining: None (Submission still accepted)\n");
-  }
-}
-
-function printSubmissionError(err) {
-  console.error(`ERROR: (${err.code})\n`);
-  console.info(err);
-  console.warn("\nYour test results have NOT been submitted!\n");
-}
